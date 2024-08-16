abaa6a49c03e16d97809cd0418bb6420f07abee2
seeleng <seelengxd@gmail.com>
2024-08-16 13:33:28 +0800

    fix: cors

--- testcase-backend/app/main.py
+++ testcase-backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import router
 from models import create_db_and_tables
 from contextlib import asynccontextmanager
+
+origins = [
+    "http://localhost:3000",
+]
 
 
 @asynccontextmanager
@@ -10,6 +15,10 @@ async def lifespan(app: FastAPI):
     yield
     # Add future cleanup code here.
 
+
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 app.include_router(router)
